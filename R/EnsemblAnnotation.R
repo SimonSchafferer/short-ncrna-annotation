08235c464aaefc233d3d109ceff059e7b3ecc1b5
SimonSchafferer <schaffrr@gmail.com>
2014-07-04 17:13:24 +0200

    test

--- R/EnsemblAnnotation.R
+++ R/EnsemblAnnotation.R
@@ -84,6 +84,7 @@ setMethod("convertRangesToDF", signature( "EnsemblAnnotation"), function(object,
 #' @rdname annotationSummary-method
 setMethod("annotationSummary", signature("EnsemblAnnotation"), function(object, ... ){
   #First defining the genome location and the rest are feature overlaps
+  #test git
   gro = annotationGR(object)
   mapgro = annotationMap(object)
   ingr = inputGR(object)
